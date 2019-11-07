1fa52d971e1907e266e8792295ea0b044b9d8a60
zheuswalker <thndrwrth@gmail.com>
2019-11-08 00:32:23 +0800

    sigin

--- mobilechecklogin.php
+++ mobilechecklogin.php
@@ -7,11 +7,13 @@ $password = $_POST['password'];
 
  $sql = "SELECT pcc_accountid FROM `philam_client_credentials` WHERE pcc_email ='".$email."' and pcc_password = (select md5('".$password."'))";
 $info = "";
-try {
+
 $result = mysqli_query($conn, $sql);
-        $info = "true";
-} catch (Exception $e) {
-    $info= "Invalid Transaction!".$e.intl_get_error_message();
+if (mysqli_num_rows($result) > 0) {
+    $info = "true";
+    }
+ else {
+    $info = "false";
 }
-echo($sql);
+echo($info);
 ?> 
